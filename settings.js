cdcce7f0430fc73c4a7a5143fce472f35d4557a1
Huguitis <65133800+Huguitis@users.noreply.github.com>
2022-11-21 12:46:57 +0100

    v1.0.0

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+  BotToken: "", 
+  BotPrefix: "!",
+  AdminIDs: "759796588842450944", // Separate them with ", "
+  ControlPanelURL: "https://huguitisnodes.host", // Control Panel url, without the last slash.
+  ControlPanelApiKey: "", // Control Panel api key.
+  SuccessEmoji: ":white_check_mark:",
+  ErrorEmoji: ":x:",
+  InfoEmoji: ":scroll:"
+}
